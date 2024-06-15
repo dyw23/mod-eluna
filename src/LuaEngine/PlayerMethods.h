f6e4bbeae005567b76fef413e6b11b79f8e24a54
Tralenor <tralenor@gmail.com>
2024-06-15 10:34:54 +0200

    fix GossipMenu Items overflow

--- src/LuaEngine/PlayerMethods.h
+++ src/LuaEngine/PlayerMethods.h
@@ -4007,7 +4007,12 @@ namespace LuaPlayer
         const char* _promptMsg = Eluna::CHECKVAL<const char*>(L, 7, "");
         uint32 _money = Eluna::CHECKVAL<uint32>(L, 8, 0);
 #if defined TRINITY || AZEROTHCORE
-        player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, _icon, msg, _sender, _intid, _promptMsg, _money, _code);
+        if (player->PlayerTalkClass->GetGossipMenu().GetMenuItemCount() < GOSSIP_MAX_MENU_ITEMS) {
+            player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, _icon, msg, _sender, _intid, _promptMsg, _money,
+                                                                 _code);
+        } else {
+            return luaL_error(L, "GossipMenuItem not added. Reached Max amount of possible GossipMenuItems.");
+        }
 #else
 #ifndef CLASSIC
         player->PlayerTalkClass->GetGossipMenu().AddMenuItem(_icon, msg, _sender, _intid, _promptMsg, _money, _code);
